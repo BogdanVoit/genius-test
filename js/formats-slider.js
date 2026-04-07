64f85f7eec4fbda1edc471e949922a62687e4e66
vn2.asu <vn2.asu@epicentrk.ua>
2026-04-07 17:44:13 +0300

    finish

---
+++ js/formats-slider.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper('.swiper-container', {  
+    loop: true, 
+  slidesPerView: 1,
+    spaceBetween: 20, 
+  breakpoints: {
+    
+    768: {
+      slidesPerView: 2,
+      
+    },
+    
+    1280: {
+      slidesPerView: 3,
+      
+    }
+  },
+  pagination: {
+      el: '.pagination',
+      bulletClass: 'pagination-button',
+      bulletActiveClass: 'pagination-button--active'
+  },  
+  navigation: {
+    nextEl: '.carusel-button.right',
+    prevEl: '.carusel-button.prev',
+  },  
+});
